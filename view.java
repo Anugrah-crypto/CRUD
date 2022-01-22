a7a341bbafd7397542fd42a534c226d88a01b4b2
Anugrah Igo Aldi Wibowo <61903779+Anugrah-crypto@users.noreply.github.com>
2022-01-22 17:39:09 +0700

    Create view.java

---
+++ view.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ 
+public class Data{
+   public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int pilih;
+        System.out.print("Pilihan:\n 1. Buat Data\n 2. Update/edit Data\n 3.Hapus Data\n 4.Cari Data\n");
+        System.out.print("Masukkan Pilihan :");
+        pilih = input.nextInt();
+
+
+    }
+}
